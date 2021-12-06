ef56f8f2a0126906f73a1cae859a439b7079d392
Midori NAKAYAMA2 <>
2021-12-06 20:02:40 +0900

    htmlソース編集しました。 ・

--- index.php
+++ index.php
@@ -12,78 +12,93 @@
 
 <body>
   <header>
-    <ul id="nav">
-        <li>Menu01</li>
-        <li>Menu02</li>
-        <li>Menu03</li>
-        <li>Menu04</li>
+    <ul id="gnav">
+        <li><a href="#">Menu01</a></li>
+        <li><a href="#">Menu02</a></li>
+        <li><a href="#">Menu03</a></li>
+        <li><a href="#">Menu04</a></li>
     </ul>
   </header>
 
   <main>
     <div id="fv_container">
-        <h1 id="logo" alt="ああああ">ロゴ</h1>
-        <div id=nav>FVエリア</div>
-        <div id="">進化をし続ける「街」アメリカンビレッジマガジン</div>
+        <h1 id="logo" alt="American Village">
+            <a href="/"><img src="assets/img/logo/logo.png"></a>
+        </h1>
+        <div id="fv">
+            <picture>
+                <source media="(min-width: 767px)" srcset="assets/img/visual/visual_1_pc.png">
+                <img src="assets/img/visual/visual_1_sp.png">
+            </picture>
+            <div id="fv_logo">
+                <picture>
+                    <source media="(min-width: 767px)" srcset="assets/img/text/visual_text_pc.png">
+                    <img src="assets/img/text/visual_text_sp.png">
+                </picture>
+            </div>
+        </div>
+        <div id="catch">進化をし続ける「街」アメリカンビレッジマガジン</div>
     </div>
     
     <div id="container">
       <h2 class="title">Latest Articles</h2>
       <div id="articles">
+        <ul>
+            <li>
+                <a href="" class="article">
+                    <p class="pic"><img src="assets/img/post/post_img_1.png"></p>
+                    <li class="date">2018/5/20</li>
+                    <li class="text">おしゃれカフェがありますよ</li>
+                    <li class="readmore">READ MORE</li>
+                </a>
+            </li>
+        
+            <li>
+                <a href="" class="article">
+                    <p class="pic"><img src="assets/img/post/post_img_2.png"></p>
+                    <li class="date">2018/5/19</li>
+                    <li class="text">あのネオンはいつ交換するのか！？観覧車の謎に迫る！</li>
+                    <li class="readmore">READ MORE</li>
+                </a>
+            </li>
 
-        <a href="" class="article">
-            <ul>
-                <p class="pic">画像</p>
-                <li class="date">2018/5/20</li>
-                <li class="text">おしゃれカフェがありますよ</li>
-                <li class="readmore">READ MORE</li>
-            </ul>
-        </a>
+            <li>
+                <a href="" class="article">
+                    <p class="pic"><img src="assets/img/post/post_img_3.png"></p>
+                    <li class="date">2018/5/18</li>
+                    <li class="text">ラソナの社内はこんなのよ</li>
+                    <li class="readmore">READ MORE</li>
+                </a>
+            </li>
 
-        <a href="" class="article">
-            <ul>
-                <p class="pic">画像e</p>
-                <li class="date">2018/5/19</li>
-                <li class="text">あのネオンはいつ交換するのか！？観覧車の謎に迫る！</li>
-                <li class="readmore">READ MORE</li>
-            </ul>
-        </a>
+            <li>
+                <a href="" class="article">
+                    <p class="pic"><img src="assets/img/post/post_img_4.png"></p>
+                    <li class="date">2018/5/17</li>
+                    <li class="text">お隣のアラハはハワイ？</li>
+                    <li class="readmore">READ MORE</li>
+                </a>
+            </li>
 
-        <a href="" class="article">
-            <ul>
-                <p class="pic">画像e</p>
-                <li class="date">2018/5/18</li>
-                <li class="text">ラソナの社内はこんなのよ</li>
-                <li class="readmore">READ MORE</li>
-            </ul>
-        </a>
+            <li>
+                <a href="" class="article">
+                    <p class="pic"><img src="assets/img/post/post_img_5.png"></p>
+                    <li class="date">2018/5/16</li>
+                    <li class="text">なぜてんと？ラソナの人に聞いてみた</li>
+                    <li class="readmore">READ MORE</li>
+                </a>
+            </li>
 
-        <a href="" class="article">
-            <ul>
-                <p class="pic">画像e</p>
-                <li class="date">2018/5/17</li>
-                <li class="text">お隣のアラハはハワイ？</li>
-                <li class="readmore">READ MORE</li>
-            </ul>
-        </a>
+            <li>
+                <a href="" class="article">
+                    <p class="pic"><img src="assets/img/post/post_img_6.png"></p>
+                    <li class="date">2018/5/15</li>
+                    <li class="text">ベイエリアおしゃれすぎる空間</li>
+                    <li class="readmore">READ MORE</li>
+                </a>
+            </li>
 
-        <a href="" class="article">
-            <ul>
-                <p class="pic">画像e</p>
-                <li class="date">2018/5/16</li>
-                <li class="text">なぜてんと？ラソナの人に聞いてみた</li>
-                <li class="readmore">READ MORE</li>
-            </ul>
-        </a>
-
-        <a href="" class="article">
-            <ul>
-                <p class="pic">画像e</p>
-                <li class="date">2018/5/15</li>
-                <li class="text">ベイエリアおしゃれすぎる空間</li>
-                <li class="readmore">READ MORE</li>
-            </ul>
-        </a>
+        </ul>
     </div>
   </main>
   
