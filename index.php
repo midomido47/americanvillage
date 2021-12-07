a05ee399a15f3731d4d09cd97686387b409b62fe
Arakaki Rina <hare00aaa@gmail.com>
2021-12-07 10:04:31 +0900

    css 書き込み

--- index.php
+++ index.php
@@ -13,95 +13,95 @@
 <body>
   <header>
     <ul id="gnav">
-        <li><a href="#">Menu01</a></li>
-        <li><a href="#">Menu02</a></li>
-        <li><a href="#">Menu03</a></li>
-        <li><a href="#">Menu04</a></li>
+        <li id="menu"><a href="#">Menu01</a></li>
+        <li id="menu"><a href="#">Menu02</a></li>
+        <li id="menu"><a href="#">Menu03</a></li>
+        <li id="menu"><a href="#">Menu04</a></li>
     </ul>
   </header>
-
-  <main>
-    <div id="fv_container">
-        <h1 id="logo" alt="American Village">
-            <a href="/"><img src="assets/img/logo/logo.png"></a>
-        </h1>
-        <div id="fv">
-            <picture>
-                <source media="(min-width: 767px)" srcset="assets/img/visual/visual_1_pc.png">
-                <img src="assets/img/visual/visual_1_sp.png">
-            </picture>
-            <div id="fv_logo">
-                <picture>
-                    <source media="(min-width: 767px)" srcset="assets/img/text/visual_text_pc.png">
-                    <img src="assets/img/text/visual_text_sp.png">
-                </picture>
+    <div id="main">
+        <main>
+            <div id="fv_container">
+                <div id="fv">
+                    <picture id="fv_img">
+                        <source media="(min-width: 767px)" srcset="assets/img/visual/visual_1_pc.png">
+                        <img src="assets/img/visual/visual_1_sp.png">
+                    </picture>
+                    <div id="fv_logo">
+                        <picture>
+                            <source media="(min-width: 767px)" srcset="assets/img/text/visual_text_pc.png">
+                            <img src="assets/img/text/visual_text_sp.png">
+                        </picture>
+                    </div>
+                </div>
+                <h1 id="logo" alt="American Village">
+                    <a href="/"><img src="assets/img/logo/logo.png"></a>
+                </h1>
+                <div id="catch">進化をし続ける「街」アメリカンビレッジマガジン</div>
             </div>
-        </div>
-        <div id="catch">進化をし続ける「街」アメリカンビレッジマガジン</div>
-    </div>
-    
-    <div id="container">
-      <h2 class="title">Latest Articles</h2>
-      <div id="articles">
-        <ul>
-            <li>
-                <a href="" class="article">
-                    <p class="pic"><img src="assets/img/post/post_img_1.png"></p>
-                    <p class="date">2018/5/20</p>
-                    <p class="text">おしゃれカフェがありますよ</p>
-                    <p class="readmore">READ MORE</p>
-                </a>
-            </li>
-        
-            <li>
-                <a href="" class="article">
-                    <p class="pic"><img src="assets/img/post/post_img_2.png"></p>
-                    <p class="date">2018/5/19</p>
-                    <p class="text">あのネオンはいつ交換するのか！？<br>観覧車の謎に迫る！</p>
-                    <p class="readmore">READ MORE</p>
-                </a>
-            </li>
+            
+            <div id="container">
+            <h2 class="title">Latest Articles</h2>
+            <div id="articles_box">
+                <ul id="articles">
+                    <li>
+                        <a href="" class="article">
+                            <p class="pic"><img src="assets/img/post/post_img_1.png"></p>
+                            <p class="date">2018/5/20</p>
+                            <p class="text">おしゃれカフェがありますよ</p>
+                            <p class="readmore">READ MORE</p>
+                        </a>
+                    </li>
+                
+                    <li>
+                        <a href="" class="article">
+                            <p class="pic"><img src="assets/img/post/post_img_2.png"></p>
+                            <p class="date">2018/5/19</p>
+                            <p class="text">あのネオンはいつ交換するのか！？<br>観覧車の謎に迫る！</p>
+                            <p class="readmore">READ MORE</p>
+                        </a>
+                    </li>
 
-            <li>
-                <a href="" class="article">
-                    <p class="pic"><img src="assets/img/post/post_img_3.png"></p>
-                    <p class="date">2018/5/18</p>
-                    <p class="text">ラソナの社内はこんなのよ</p>
-                    <p class="readmore">READ MORE</p>
-                </a>
-            </li>
+                    <li>
+                        <a href="" class="article">
+                            <p class="pic"><img src="assets/img/post/post_img_3.png"></p>
+                            <p class="date">2018/5/18</p>
+                            <p class="text">ラソナの社内はこんなのよ</p>
+                            <p class="readmore">READ MORE</p>
+                        </a>
+                    </li>
 
-            <li>
-                <a href="" class="article">
-                    <p class="pic"><img src="assets/img/post/post_img_4.png"></p>
-                    <p class="date">2018/5/17</p>
-                    <p class="text">お隣のアラハはハワイ？</p>
-                    <p class="readmore">READ MORE</p>
-                </a>
-            </li>
+                    <li>
+                        <a href="" class="article">
+                            <p class="pic"><img src="assets/img/post/post_img_4.png"></p>
+                            <p class="date">2018/5/17</p>
+                            <p class="text">お隣のアラハはハワイ？</p>
+                            <p class="readmore">READ MORE</p>
+                        </a>
+                    </li>
 
-            <li>
-                <a href="" class="article">
-                    <p class="pic"><img src="assets/img/post/post_img_5.png"></p>
-                    <p class="date">2018/5/16</p>
-                    <p class="text">なぜてんと？ラソナの人に聞いてみた</p>
-                    <p class="readmore">READ MORE</p>
-                </a>
-            </li>
+                    <li>
+                        <a href="" class="article">
+                            <p class="pic"><img src="assets/img/post/post_img_5.png"></p>
+                            <p class="date">2018/5/16</p>
+                            <p class="text">なぜてんと？ラソナの人に聞いてみた</p>
+                            <p class="readmore">READ MORE</p>
+                        </a>
+                    </li>
 
-            <li>
-                <a href="" class="article">
-                    <p class="pic"><img src="assets/img/post/post_img_6.png"></p>
-                    <p class="date">2018/5/15</p>
-                    <p class="text">ベイエリアおしゃれすぎる空間</p>
-                    <p class="readmore">READ MORE</p>
-                </a>
-            </li>
-        </ul>
-        </div>
-    </div>
-  </main>
-  
+                    <li>
+                        <a href="" class="article">
+                            <p class="pic"><img src="assets/img/post/post_img_6.png"></p>
+                            <p class="date">2018/5/15</p>
+                            <p class="text">ベイエリアおしゃれすぎる空間</p>
+                            <p class="readmore">READ MORE</p>
+                        </a>
+                    </li>
+                </ul>
+                </div>
+            </div>
+        </main>
+  </div>
   <footer>
     <p>Copyright © Shimobaru.All RIGHT RESERVED.</p>
   </footer>
