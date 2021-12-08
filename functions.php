eddc5d78e52fb3d66f123c21bd050fab2b7465cc
Arakaki Rina <hare00aaa@gmail.com>
2021-12-08 15:17:32 +0900

    競合解決

--- functions.php
+++ functions.php
@@ -1,15 +1,6 @@
 <?php
   // ページのメニューを管理画面で操作できるために機能
-<<<<<<< HEAD
- add_theme_support('menus');
-  //　投稿ページにサムネイルを追加する方法
-  add_theme_support('post-thumbnails');
-  // 投稿ページのアーカイブページを作成する
-  function post_has_archive($args,$post_type){
-    if('post'== $post_type){
-       $args['rewrite'] = true;
-       $args['has_archive'] = 'blog';
-=======
+
   add_theme_support('menus');
   // 投稿ページにサムネイルを追加する方法
   add_theme_support('post-thumbnails');
@@ -18,7 +9,6 @@
     if ('post' == $post_type) {
       $args['rewrite'] = true; //リライトを有効にする
       $args['has_archive'] = 'archive'; //任意のスラッグ名
->>>>>>> e6fe70e5da3881e1d9b44ffdea77e5fbd745f6ed
     }
     return $args;
   }
